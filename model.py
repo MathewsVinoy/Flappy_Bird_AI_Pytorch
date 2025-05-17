7d65a2882275d4c337cb7d97d057077cea1ea1fd
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-17 23:45:50 +0530

    update

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
+import os
 import torch
 from torch import nn
 
 class Model(nn.Model):
-    def __init__(input_size, output_size, hidden_size):
+    def __init__(self,input_size, output_size, hidden_size):
         super(Model, self).__init__()
         self.seq = nn.Sequential(
             nn.Linear(in_features=input_size, out_features=hidden_size),
@@ -11,3 +12,11 @@ class Model(nn.Model):
         
     def forward(self,x):
         return self.seq(x)
+    
+    def save(self, file_name='model.pth'):
+        model_folder_path='./model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
