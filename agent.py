80e67167c8ef21af50445329cbe7dc29fa0bef7f
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-17 23:29:58 +0530

    added long memmery

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from collections import deque
 from model import Model
 from game import Bird
+import random
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -18,3 +19,12 @@ class Agent:
 
     def remember(self,state, action, reward, next_state, done):
         self.memory.append((state,action,reward,next_state,done))
+
+    def train_long_memory(self):
+        if len(self.memory)>BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample=self.memory
+
+        states,actions,rewards,next_states,dones=zip(*mini_sample)
+        self.trainer.train_step(states,actions,rewards,next_states,dones)
