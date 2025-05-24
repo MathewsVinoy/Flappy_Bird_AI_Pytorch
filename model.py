324b0d8e8412c8e14861268b3274f3f2b6abc311
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-24 20:54:52 +0530

    updated compleated

--- model.py
+++ model.py
@@ -1,19 +1,20 @@
 import os
 import torch
 from torch import nn
+import torch.nn.functional as F
 
-class Model(nn.Model):
-    def __init__(self,input_size, output_size, hidden_size):
-        super(Model, self).__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(in_features=input_size, out_features=hidden_size),
-            nn.Linear(in_features=hidden_size, out_features=output_size),
-        )
-        
+class Model(nn.Module):
+    def __init__(self,input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
     def forward(self,x):
-        return self.seq(x)
-    
-    def save(self, file_name='model.pth'):
+        x= F.relu(self.linear1(x))
+        x=self.linear2(x)
+        return x
+
+    def save(self, file_name='model1.pth'):
         model_folder_path='./model'
         if not os.path.exists(model_folder_path):
             os.makedirs(model_folder_path)
