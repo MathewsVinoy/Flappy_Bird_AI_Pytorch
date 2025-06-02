29d2670e960ce2d0f0a374fb6b310e6597ed1fc0
MathewsVinoy <mathewsvinoy@gmail.com>
2025-06-02 11:36:44 +0530

    update we more the one birds

--- agent.py
+++ agent.py
@@ -1,9 +1,10 @@
-from collections import deque
-from model import Model
-from game import GamePlay
-import random
-from Qtrainer import QTrainer
 import torch
+import random
+from model import Model
+from Qtrainer import QTrainer
+from collections import deque
+from gameplay import GamePlay
+
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -16,15 +17,10 @@ class Agent:
         self.gamma = 0.9
         self.memory = deque(maxlen=MAX_MEMORY)
         self.model = Model(3,1024,2)
-        # Todo : create and import trainer class
         self.trainer =QTrainer(self.model, lr=LR, gamma=self.gamma)
-
 
     def remember(self,state, action, reward, next_state, done):
         self.memory.append((state,action,reward,next_state,done))
-    
-    def clean_memory(self):
-        pass
 
     def train_long_memory(self):
         if len(self.memory)>BATCH_SIZE:
@@ -39,46 +35,44 @@ class Agent:
         self.trainer.train_step(state,action,reward,next_state,done)
 
     def get_action(self,state):
-        # random moves : explotion or exploration
         self.epsilon = 80-self.n_game
-        final_move =[0,0]
-        if random.randint(0,200) <self.epsilon:
-            move = random.randint(0,1)
-            final_move[move]=1
-        else:
-            state0=torch.tensor(state, dtype=torch.float)
-            prediction = self.model(state0)
-            move = torch.argmax(prediction).item()
-            final_move[move]=1
-        return final_move
-
-
+        output = []
+        for i in range(len(state)):
+            final_move =[0,0]
+            if random.randint(0,200) <self.epsilon:
+                move = random.randint(0,1)
+                final_move[move]=1
+            else:
+                state0=torch.tensor(state[i], dtype=torch.float)
+                prediction = self.model(state0)
+                move = torch.argmax(prediction).item()
+                final_move[move]=1
+            output.append(final_move)
+        return output
     
+
+
 def train():
-    total_score =0
     record =0
-    birds=[]
     agent = Agent()
     game = GamePlay()
     while True:
-        for i in range(10):
-            state_old = game.get_state()
-            # print(state_old)
-            final_move = agent.get_action(state_old)
-            # print(final_move)
-            reward, done, score = game.play_step(final_move)
-            state_new = game.get_state()
-            agent.train_short_memory(state_old, final_move, reward, state_new,done)
-            agent.remember(state_old,final_move,reward,state_new,done)
-            if done:
-                game.reset()
-                agent.n_game +=1
-                agent.train_long_memory()
+        state_old = game.get_state()
+        final_move = agent.get_action(state_old)
+        score, birds = game.play_step(final_move)
+        state_new = game.get_state()
+        for bird in birds:
+            agent.train_short_memory(state_old, final_move, bird.reward, state_new,bird.game_over)
+            agent.remember(state_old,final_move,bird.reward,state_new,bird.game_over)
+        if len(birds)==0:
+            game.reset()
+            agent.n_game +=1
+            agent.train_long_memory()
 
-                if score > record:
-                    record = score
-                    agent.model.save()
-                print("Game ",agent.n_game, " Score ",score," Record ", record, "Reward: ", reward)
+            if score > record:
+                record = score
+                agent.model.save()
+            print("Game ",agent.n_game, " Score ",score," Record ", record)
     
 
 if __name__ == "__main__":
